c290cb7403e45d829f9afb1f14317c16c31851b8
ZhangYawen <wes214@163.com>
2023-04-25 09:14:01 +0300

    Initial

---
+++ js/HelloWorld/HelloWorld.js
@@ -0,0 +1,8 @@
+console.log("Hello World JavaScript!");
+
+let n = 10;
+const names = "wes";
+const list = [1, 2, 3];
+console.log(n);
+console.log(names);
+console.log(list);
---
+++ python/HelloWorld/HelloWorld.py
@@ -0,0 +1,14 @@
+
+print("Hello World Python!")
+
+
+counter = 100
+name = "Wes"
+print(counter)
+print(name)
+
+
+matrix = [0, 1, 2, 3]
+print(matrix)
+matrix = [0 for i in range(4)]
+print(matrix)
