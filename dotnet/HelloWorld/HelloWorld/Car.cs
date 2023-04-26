409018ebff1abda9123fd04a0ac24fcceda2b7b1
ZhangYawen <wes214@163.com>
2023-04-26 10:56:36 +0300

    object oriented programming

---
+++ dotnet/HelloWorld/HelloWorld/Car.cs
@@ -0,0 +1,17 @@
+namespace MyModule
+{
+    public class Car
+    {
+
+        public string Model { get; set; }
+
+        public string Make { get; set; }
+
+        public int CurrentSpeed { get; set; }
+
+        public bool EngineRunning { get; set; }
+
+
+    }
+
+}
---
+++ dotnet/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Console.WriteLine("Hello, World!");
+
+string ss = "AABBC";
+
+for (int i = 0; i < ss.Length; i++)
+{
+    Console.WriteLine(ss[i]);
+    Console.WriteLine(ss[ss.Length - 1 - i]);
+    Console.WriteLine("--------------");
+}
+
+*/
+
+
+string name = "Wes";
+
+Car myCar = new Car();
+Car myCar2 = new();
+
+myCar.Make = "Toyota";
+
