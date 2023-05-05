2ed29c89a5a7d3f0a27573d8b66d9841345bc1f7
ZhangYawen <wes214@163.com>
2023-05-05 14:27:34 +0300

    update database sql

--- database/practice1/practice1.sql
+++ database/practice1/practice1.sql
@@ -1,18 +1,22 @@
 
 -- part1
 
+-- All Finnish customers
 select * from customers where Country='Finland';
 
-
+-- All orders for the customer ”Que Delícia”
 select ord.* from Orders as  ord left join customers as customer on ord.CustomerID=customer.CustomerID  
 where customer.CompanyName='Que Delícia';
 
+-- All employees from London
 select * from Employees where city='London';
 
 -- part2
 
+-- How many customers does Northwind have?
 select count(1) from customers;
 
+-- How much tofu (in $) has Northwind sold?
 select sum(detail.UnitPrice * detail.Quantity - detail.Discount) 
     from 'Order Details' as detail left join Products as product on detail.ProductID=product.ProductID 
     where product.ProductName='Tofu';
