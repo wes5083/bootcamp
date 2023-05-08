4c3c687180e9de369fed5a09a7231635813e35ae
ZhangYawen <wes214@163.com>
2023-05-08 13:27:19 +0300

    add choise to show the result

--- js/NodeJS/NodeSQLite/sqlite.js
+++ js/NodeJS/NodeSQLite/sqlite.js
@@ -1,8 +1,32 @@
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("../../../database/northwind.db");
-listFinnishCustomers();
-listLondonCustomers();
-listTofuSales();
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+readline.question(
+  `Please select your choice:
+1. List Finnish customers
+2. List London employees
+3. List Tofu sales.
+Please enter your choice (1-3): `,
+  (choice) => {
+    console.log(`You selected choice ${choice}.`);
+    readline.close();
+    switch (choice) {
+      case "1":
+        listFinnishCustomers();
+        break;
+      case "2":
+        listLondonCustomers();
+        break;
+      case "3":
+        listTofuSales();
+        break;
+    }
+  }
+);
 
 function listFinnishCustomers() {
   const sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
