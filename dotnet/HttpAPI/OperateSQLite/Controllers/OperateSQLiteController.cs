1aa18c82efd5e1133623b3529f5be7f7d98cf536
ZhangYawen <wes214@163.com>
2023-05-10 15:13:15 +0300

    C# HTTP API Operate SQLite

---
+++ dotnet/HttpAPI/OperateSQLite/Controllers/OperateSQLiteController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+
+namespace OperateSQLite.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OperateSQLiteController : ControllerBase
+{
+
+    [Route("GetFinnishCustomers")]
+    public List<string> GetFinnishCustomers()
+    {
+
+        SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\00_zyw\\500_Bootcamp\\bootcamp\\database\\northwind.db");
+        connection.Open();
+
+        string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+
+        SQLiteCommand command = new SQLiteCommand(sql, connection);
+        SQLiteDataReader reader = command.ExecuteReader();
+
+        List<string> result = new List<string>();
+        while (reader.Read())
+        {
+            string companyName = reader["CompanyName"]?.ToString() ?? "";
+            Console.WriteLine(companyName);
+            result.Add(companyName);
+        }
+
+        reader.Close();
+        connection.Close();
+        return result;
+    }
+
+
+    [Route("AddFinnishCustomers")]
+    public string AddFinnishCustomers()
+    {
+
+        SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\00_zyw\\500_Bootcamp\\bootcamp\\database\\northwind.db");
+        connection.Open();
+
+        string sql = "insert into " +
+        "Customers('CustomerID','CompanyName','ContactName','ContactTitle','Address','City','Region','PostalCode','Country','Phone','Fax')" +
+        "values" +
+        "('ZYW_ID', 'ZYW', 'zyw_contractname', 'zyw_contracttitle', 'Espoo 001', 'Espoo', 'Espoo', 40012, 'Finland', '90-2248858', '90-224 8858')";
+
+        SQLiteCommand command = new SQLiteCommand(sql, connection);
+        command.ExecuteNonQuery();
+        connection.Close();
+        return "success";
+    }
+
+}
---
+++ dotnet/HttpAPI/OperateSQLite/README.md
@@ -0,0 +1,9 @@
+# Pratice 2. HTTP APIs with databases.
+
+### Continue the development of your API and connect the API with a database.
+
+### For example, use the C# code from previous training days to access the Northwind database.
+
+### Then, implement an API endpoint that can read (for example) Northwind customer data.
+
+### Bonus: how could you implement a API endpoint that writes new customer information to the database?
