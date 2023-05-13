e8163746636502f98f4ec2527dac51e8e96cc870
ZhangYawen <wes214@163.com>
2023-05-13 15:16:01 +0300

    Python Class

---
+++ python/Basic/11_class.py
@@ -0,0 +1,11 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+
+print(student1.name)
+print(student1.gpa)
+print(student2.name)
+print(student2.gpa)
+
+
