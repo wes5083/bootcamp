f9e1fb7c1b0b279958641551c0bcfe3375c9483d
ZhangYawen <wes214@163.com>
2023-05-13 15:48:15 +0300

    Python Object function of Class

--- python/Basic/11_class.py
+++ python/Basic/11_class.py
@@ -1,11 +1,14 @@
 from Student import Student
 
 student1 = Student("Jim", "Business", 3.1, False)
-student2 = Student("Pam", "Art", 2.5, True)
+student2 = Student("Pam", "Art", 4.5, True)
 
 print(student1.name)
 print(student1.gpa)
 print(student2.name)
 print(student2.gpa)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
 
 
