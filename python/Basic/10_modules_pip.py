13fb6324bd7fc70adb03dc59a5fe2c525221443c
ZhangYawen <wes214@163.com>
2023-05-14 09:01:14 +0300

    Python Django HelloWorld

--- python/Basic/10_modules_pip.py
+++ python/Basic/10_modules_pip.py
@@ -2,8 +2,13 @@ import docx
 
 '''
 pip --version
-pip install python-docx
+pip install python-docx  
+pip install django
 '''
+
+
+# django project
+# django-admin startproject DjangoHelloWorld
 
 print(docx.api)
 
