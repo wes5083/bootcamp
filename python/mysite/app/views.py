7de3d181f5c6f46050911f8a92406cdd46bf1e48
ZhangYawen <wes214@163.com>
2023-05-14 21:02:11 +0300

    Django Init index

--- python/mysite/app/views.py
+++ python/mysite/app/views.py
@@ -3,3 +3,45 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     # return HttpResponse("success")
     return render(request, "index.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
