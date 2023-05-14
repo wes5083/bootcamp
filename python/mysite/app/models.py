1fb51afc047a1d9af224363867dfcb6a074f30ba
ZhangYawen <wes214@163.com>
2023-05-14 22:12:24 +0300

    Django MySQL Create tables

--- python/mysite/app/models.py
+++ python/mysite/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField(default=1)
+
+
+class Department(models.Model):
+    title = models.CharField(max_length=64)
