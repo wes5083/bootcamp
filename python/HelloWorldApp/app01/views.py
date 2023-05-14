d6720fef6572ceaf29e12e62f59380c35548e84a
ZhangYawen <wes214@163.com>
2023-05-14 10:32:00 +0300

    Python Django Params & templates grammar

--- python/HelloWorldApp/app01/views.py
+++ python/HelloWorldApp/app01/views.py
@@ -9,7 +9,16 @@ def index(request):
 
 
 def user_list(request):
-    return render(request, "user_list.html")
+    name = "Wes"
+    roles = ["admin", "guest"]
+    user = {"name": "Wes", "salary": 1000, "role": "admin"}
+    user_lists = [
+        {"name": "Wes1", "salary": 1000, "role": "admin"},
+        {"name": "Wes2", "salary": 2000, "role": "guest"},
+        {"name": "Wes3", "salary": 3000, "role": "admin"},
+
+    ]
+    return render(request, "user_list.html", {"name": name, "role": roles, "user": user,"user_lists":user_lists})
 
 
 def user_add(request):
