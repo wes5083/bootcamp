8c0a857493dab42243e8ea83eae29e90245afe92
ZhangYawen <wes214@163.com>
2023-05-15 10:27:21 +0300

    Django Employee Depart list

--- python/employee/app/views.py
+++ python/employee/app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+
+def depart_list(request):
+    return render(request, "depart_list.html")
