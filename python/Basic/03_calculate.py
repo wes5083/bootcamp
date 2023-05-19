b10ffeea13fbcaba0b767812368fed448067f17e
ZhangYawen <wes214@163.com>
2023-05-19 23:50:22 +0300

    Python Simple operate

--- python/Basic/03_calculate.py
+++ python/Basic/03_calculate.py
@@ -1,3 +1,24 @@
+
+print(1+1)
+print(1-1)
+print(1*2)
+print(11/2)     # 5.5
+print(11//2)    # 5 get int
+print(11%2)     # 1
+print(2**3)     # 2*2*2=8
+
+
+print(9//4)     # 2
+print(-9//-4)   # 2
+# get integer down
+print(9//-4)    # -3
+print(-9//4)    # -3
+
+# remainder = dividend - divisor * quotient
+print(9%-4)     # -3    9-(-4)*(-3) = -3
+print(-9%4)     # 3     -9-(4)*(-3) = 3
+
+
 # Basic calculate
 print("----------------Basic calculate-----------------")
 
