f29d16a9927f2da7e7a2f796b9908d418130e378
ZhangYawen <wes214@163.com>
2023-05-20 00:32:28 +0300

    Python bool value

---
+++ python/Basic/03_bool.py
@@ -0,0 +1,20 @@
+print("----------bool value is False-----------------")
+print(bool(False))  # False bool value is False
+print(bool(0))      # int 0
+print(bool(0.0))    # float 0
+print(bool(None))   # None
+print(bool(''))     # null character
+print(bool(""))     # null str
+print(bool([]))     # None array
+print(bool(list())) # None list
+print(bool(()))     # None tuple
+print(bool({}))     # None tuple
+print(bool(dict())) # None dict
+print(bool(set()))  # None set
+
+
+print("----------Other object bool value is True-----------------")
+
+print(bool(8))
+print(bool(True))
+print(bool("test"))
