0c109e992c7b8e5481b94267dd213fff65b1ba5a
ZhangYawen <wes214@163.com>
2023-05-19 23:53:59 +0300

    Python assignment

--- python/Basic/03_calculate.py
+++ python/Basic/03_calculate.py
@@ -1,23 +1,25 @@
+print(1 + 1)
+print(1 - 1)
+print(1 * 2)
+print(11 / 2)  # 5.5
+print(11 // 2)  # 5 get int
+print(11 % 2)  # 1
+print(2 ** 3)  # 2*2*2=8
 
-print(1+1)
-print(1-1)
-print(1*2)
-print(11/2)     # 5.5
-print(11//2)    # 5 get int
-print(11%2)     # 1
-print(2**3)     # 2*2*2=8
-
-
-print(9//4)     # 2
-print(-9//-4)   # 2
+print(9 // 4)  # 2
+print(-9 // -4)  # 2
 # get integer down
-print(9//-4)    # -3
-print(-9//4)    # -3
+print(9 // -4)  # -3
+print(-9 // 4)  # -3
 
 # remainder = dividend - divisor * quotient
-print(9%-4)     # -3    9-(-4)*(-3) = -3
-print(-9%4)     # 3     -9-(4)*(-3) = 3
+print(9 % -4)  # -3    9-(-4)*(-3) = -3
+print(-9 % 4)  # 3     -9-(4)*(-3) = 3
 
+a = b = c = 20  # assignment
+print(a, id(a))
+print(b, id(b))
+print(c, id(c))
 
 # Basic calculate
 print("----------------Basic calculate-----------------")
