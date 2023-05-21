cf0c8956fefadadf30d3153a1b33a39c8923e3df
ZhangYawen <wes214@163.com>
2023-05-21 07:59:19 +0300

    Python while / for in

--- python/Basic/04_python_loop.py
+++ python/Basic/04_python_loop.py
@@ -10,8 +10,41 @@ print(list(r))  # [1...9]
 r = range(1, 10, 2)
 print(list(r))  # [1,3,5,7,9]
 
+for i in range(1,4):
+    for j in range(1,5):
+        print('*', end='\t')
+    print()
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i, '*', j, '=', i*j, '  ', end='')
+    print()
+
+
 # in, not in
 print(10 in r)
 print(9 in r)
 print(10 not in r)
 print(9 not in r)
+
+# while loop / for in
+a = 1
+while a < 10:
+    print(a)
+    a += 1
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[0][0])  # 1
+print(number_grid[2][1])  # 8
+
+for row in number_grid:
+    print(row)
+    for col in row:
+        print(col)
+
