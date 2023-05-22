f2c6f658ad1bbda9fd2d672b8b0fad5c8c89995b
ZhangYawen <wes214@163.com>
2023-05-22 07:17:18 +0300

    Python list

--- python/Basic/04_list.py
+++ python/Basic/04_list.py
@@ -12,9 +12,16 @@ print(lst.index('hello'))
 print(lst.index('hello', 1, 4))
 print(lst[1], lst[-1])
 
-lst3 = [10, 20, 30, 40, 5, 60]
+lst3 = [10, 20, 30, 40, 50, 60, 70, 80]
 print(lst3[1:6])  # start 1 step 1
 print(lst3[1:6:])  # start 1 step 1
 print(lst3[1:6:2])  # start 1 step 2
 print(lst3[:6:2])  # start 0 step 2
 print(lst3[1::2])  # start 1 step 2
+
+print('original list:', lst3)
+print(lst3[::-1])  # start=0, stop=0, step=-1
+print(lst3[7::-1])  # start=7, stop=0, step=-1
+print(lst3[6:0:-2])  # start=6, stop=0, step=-2
+
+print('p' in 'python')  # true
