83b47a18dcb5f1f63262faf3ee9b091a2304e814
ZhangYawen <wes214@163.com>
2023-05-23 08:36:32 +0300

    Python dict

---
+++ python/Basic/04_tuple.py
@@ -0,0 +1,37 @@
+t = ('Hello', 'World', 'Python')
+print(t, type(t))
+t = ('Hello',)
+print('-----only one element-----', t, type(t))
+t = ('Hello')
+print('-----only one element-----', t, type(t))
+
+t = tuple(('Hello', 'World', 'Python'))
+print(t, type(t))
+t = 'Hello', 'World', 'Python'
+print(t, type(t))
+
+print(t[0])
+print(t[1])
+print(t[2])
+
+for item in t:
+    print(item)
+
+lst = []
+lst1 = list()
+d = {}
+d2 = dict()
+t = ()
+t2 = tuple()
+
+print()
+
+lst = [10, 20, 30]
+print(id(str))
+lst.append(100)
+print(id(str))
+
+s = 'hello'
+print(id(s))
+s = s + ' world'
+print(id(s))
