ad9ac234373fdb91718bab4d105a553a965ac329
ZhangYawen <wes214@163.com>
2023-05-24 09:07:35 +0300

    Python String

---
+++ python/Basic/06_String.py
@@ -0,0 +1,12 @@
+s1 = 'aaa'
+s2 = 'aaa'
+print(s1 is s2)
+
+s = 'hello, hello'
+print(s.index('lo'))
+print(s.find('lo'))
+print(s.rindex('lo'))
+print(s.rfind('lo'))
+
+print(s.find('k'))
+print(s.find('k'))
