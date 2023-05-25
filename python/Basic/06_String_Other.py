0bd862e25254cead94a8e5d99f1713a05a01cc9a
ZhangYawen <wes214@163.com>
2023-05-26 00:19:48 +0300

    Python String

---
+++ python/Basic/06_String_Other.py
@@ -0,0 +1,17 @@
+print('apple' > 'app')  # True
+print('apple' > 'banana')  # False
+print(ord('a'), ord('b'))
+print(chr(97), chr(98))
+
+a = b = 'Python'
+c = 'Python'
+print(a == b)  # True
+print(a == c)  # True
+print(a is b)  # True
+print(a is c)  # True
+print(id(a), id(b), id(c))
+
+
+
+
+
