f29a936eadabf271b8ad31825d6ba2a184b75f36
ZhangYawen <wes214@163.com>
2023-05-28 21:28:21 +0300

    Python default object

--- python/BasicObject/InheritanceMulty.py
+++ python/BasicObject/InheritanceMulty.py
@@ -1,5 +1,9 @@
 class A(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'My name is {0}'.format(self.name)
 
 
 class B(object):
@@ -8,3 +12,10 @@ class B(object):
 
 class C(A, B):
     pass
+
+
+# object default is all the class's father
+a = A('zhangsan')
+print(dir(a))
+print(a)
+print(type(a))
