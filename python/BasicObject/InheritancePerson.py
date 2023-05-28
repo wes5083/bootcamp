7edd4fae4c305add6c157ce00ebb1e5c38e7947b
ZhangYawen <wes214@163.com>
2023-05-28 21:22:21 +0300

    Python method overloader

--- python/BasicObject/InheritancePerson.py
+++ python/BasicObject/InheritancePerson.py
@@ -12,11 +12,19 @@ class Student(Person):
         super().__init__(name, age)
         self.stu_no = stu_no
 
+    def info(self):  # method overload
+        super().info()
+        print('student no: ',self.stu_no)
+
 
 class Teacher(Person):
-    def __init__(self, name, age, tech_year):
+    def __init__(self, name, age, teach_year):
         super().__init__(name, age)
-        self.tech_year = tech_year
+        self.teach_year = teach_year
+
+    def info(self):  # method overload
+        super().info()
+        print('teach_year: ',self.teach_year)
 
 
 stu = Student('zhangsan', 20, '1001')
