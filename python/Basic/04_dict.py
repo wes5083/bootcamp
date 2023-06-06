c5888cc318d65d22ed60a1e469f5f42b0a440e56
ZhangYawen <wes214@163.com>
2023-06-06 22:30:04 +0300

    Python lambda of dictionary

--- python/Basic/04_dict.py
+++ python/Basic/04_dict.py
@@ -29,3 +29,30 @@ item = ['fruits', 'books', 'others']
 prices = [96, 78, 85]
 d = {item: price for item, price in zip(items, prices)}
 print(d)
+
+'''
+dictionary = {key: expression for (key, value) in iterable}
+dictionary = {key: expression for (key, value) in iterable if conditional}
+dictionary = {key: (if/else) for (key, value) in iterable}
+dictionary = {key: function(value) for (key, value) in iterable}
+'''
+weather_in_F = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+weather_in_C = {key: round((value - 32) * (5 / 9)) for (key, value) in weather_in_F.items()}
+print(weather_in_C)
+weather = {'New York': 'snowing', 'Boston': 'sunny', 'Los Angeles': 'sunny', 'Chicago': 'cloudy'}
+weather_sunny = {key: value for (key, value) in weather.items() if value == "sunny"}
+print(weather_sunny)
+weather_desc = {key: ("WARM" if value >= 40 else "COLD") for (key, value) in weather_in_F.items()}
+print(weather_desc)
+
+
+def check_temp(value):
+    if value >= 70:
+        return "HOT"
+    elif 69 >= value >= 40:
+        return "WARM"
+    else:
+        return "CODE"
+
+weather_function = {key: check_temp(value) for (key, value) in weather_in_F.items()}
+print(weather_function)
